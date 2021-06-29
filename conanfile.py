405c63641a7d18dc15ebce45230fff77f06f9999
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-06-29 14:46:49 +0200

    synchronize with CCI

--- conanfile.py
+++ conanfile.py
@@ -1,5 +1,4 @@
 import os
-
 from conans import ConanFile, CMake, tools
 
 class MinizConan(ConanFile):
@@ -11,7 +10,7 @@ class MinizConan(ConanFile):
     topics = ("conan", "miniz", "compression", "lossless")
     homepage = "https://github.com/richgel999/miniz"
     url = "https://github.com/conan-io/conan-center-index"
-    exports_sources = "CMakeLists.txt"
+    exports_sources = "CMakeLists.txt", "patches/*"
     generators = "cmake"
     settings = "os", "arch", "compiler", "build_type"
     options = {"shared": [True, False], "fPIC": [True, False]}
@@ -39,16 +38,18 @@ class MinizConan(ConanFile):
         tools.get(**self.conan_data["sources"][self.version])
         os.rename(self.name + "-" + self.version, self._source_subfolder)
 
-    def build(self):
-        cmake = self._configure_cmake()
-        cmake.build()
-
     def _configure_cmake(self):
         if self._cmake:
             return self._cmake
         self._cmake = CMake(self)
         self._cmake.configure(build_folder=self._build_subfolder)
         return self._cmake
+
+    def build(self):
+        for patch in self.conan_data.get("patches", {}).get(self.version, []):
+            tools.patch(**patch)
+        cmake = self._configure_cmake()
+        cmake.build()
 
     def package(self):
         self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
